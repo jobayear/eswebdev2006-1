f57b6306201d198d648133a41fbd6a0a40184e3f
jobayear <jobayear@users.noreply.github.com>
2023-03-19 22:55:29 +0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP Variable Example</title>
+</head>
+<body>
+	<?php
+		$my_variable = 56;
+		echo "The value of my variable is: " . $my_variable;
+	?>
+</body>
+</html>
